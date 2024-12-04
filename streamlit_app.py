8072ded343a3d4b85a142a61d29970fa859ef60a
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-12-03 16:54:27 -0800

    file path

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ def process_and_plot(dataset, title):
 # Load and process data
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/economic_results.csv")
+    return pd.read_csv("economic_results.csv")
 
 df = load_data()
 
